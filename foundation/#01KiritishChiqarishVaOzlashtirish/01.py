2d6f4feddd7b57529c1d4596f17b01635a34808a
Dastonbek Yakubov <yakubovdastonbek@gmail.com>
2024-07-28 23:54:37 +0500

    "foundation fayli qo'shildi"

---
+++ foundation/#01KiritishChiqarishVaOzlashtirish/01.py
@@ -0,0 +1,8 @@
+#1.Kvadratning tomoni a berilgan. P = 4 * a formula orqali perimetri aniqlansin.
+
+#Bu masalani yechishda a malum son bolganligi uchun a = son berib ketishimiz mumkin
+#yoki input metodi yordamida foydalanuvchidan qabul qilib olishimiz mumkin.
+
+son = int(input("a = "))
+perimetr = 4*son
+print(perimetr)
